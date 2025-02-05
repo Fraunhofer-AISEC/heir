55ef9391dc5b82ad493c8e0c6d2ae5e00f939831
cstarn <36639566+CStarn@users.noreply.github.com>
2025-02-05 12:53:35 +0100

    Add ring dimension selection

--- lib/Analysis/OperationCountAnalysis/OperationCountAnalysis.cpp
+++ lib/Analysis/OperationCountAnalysis/OperationCountAnalysis.cpp
@@ -4,6 +4,8 @@
 
 #include <cassert>
 #include <cstdint>
+#include <cstdio>
+#include <iostream>
 
 #include "OperationCountAnalysis.h"
 #include "lib/Analysis/LevelAnalysis/LevelAnalysis.h"
@@ -13,6 +15,8 @@
 #include "lib/Dialect/Secret/IR/SecretOps.h"
 #include "src/core/include/math/hal/vector.h"                //from @openfhe
 #include "src/core/include/math/nbtheory.h"                //from @openfhe
+#include "src/core/include/lattice/stdlatticeparms.h"       //from @openfhe
+#include "src/pke/include/scheme/scheme-utils.h"            //from @openfhe
 #include "src/core/include/math/hal/nativeintbackend.h"        //from @openfhe
 #include "llvm/include/llvm/ADT/TypeSwitch.h"              // from @llvm-project
 #include "llvm/include/llvm/Support/Debug.h"               // from @llvm-project
@@ -28,6 +32,8 @@
 
 namespace mlir {
 namespace heir {
+
+constexpr int MAX_BIT_SIZE = 60;
 
 LogicalResult OperationCountAnalysis::visitOperation(
     Operation *op, 
@@ -122,7 +128,7 @@ static uint64_t computeModulusOrder(int ringDimension, uint64_t plaintextModulus
 static uint64_t findValidFirstModSize(int minModSize, int ringDimension, int plaintextModulus) {
   uint64_t modulusOrder = computeModulusOrder(ringDimension, plaintextModulus);
 
-  while (minModSize < 60) {
+  while (minModSize < MAX_BIT_SIZE) {
     try {
       lbcrypto::LastPrime<lbcrypto::NativeInteger>(minModSize, modulusOrder);
       return minModSize;
@@ -141,7 +147,7 @@ static uint64_t findValidScalingModSize(int minModSize, int firstModSize, int nu
     firstModulus = lbcrypto::LastPrime<lbcrypto::NativeInteger>(firstModSize, modulusOrder);
   }
 
-  while (minModSize < 60) {
+  while (minModSize < MAX_BIT_SIZE) {
     try {
       auto q = lbcrypto::LastPrime<lbcrypto::NativeInteger>(minModSize, modulusOrder);
       for (int i = 1; i < numPrimes; i++) {
@@ -159,11 +165,36 @@ static uint64_t findValidScalingModSize(int minModSize, int firstModSize, int nu
   return 0;
 }
 
+static int computeOptimalBoundSize(int ringDimension, int plaintextModulus, int maxKeySwitchCount, int maxCiphertextCount, int numPrimes) {
+      auto phi = ringDimension;
+      auto beta = pow(2.0, 10); // TODO Replace by value set through developer
+      auto t = plaintextModulus;
+      auto D = 6.0;
+
+      auto vKey = 2.0 / 3.0; // TODO Make adjustable
+      auto vErr = 3.19 * 3.19; // TODO Make adjustable
+
+      auto boundScale = D * t * sqrt((phi / 12.0) * (1.0 + (phi * vKey)));
+
+      auto boundKeySwitch = D * t * phi * sqrt(vErr / 12.0);
+
+      auto K = 100.0;
+      auto P = K * beta * sqrt(log(numPrimes * pow(2.0, MAX_BIT_SIZE)) / log(beta));
+
+      auto f0 = beta * sqrt(numPrimes * log2(t * phi)) / P;
+
+      auto vKS = f0 * boundKeySwitch + boundScale;
+
+      return log2(boundScale + sqrt(boundScale * boundScale + (maxKeySwitchCount * vKS)));
+}
+
 void annotateCountParams(Operation *top, DataFlowSolver *solver,
                          int ringDimension, int plaintextModulus) {
   top->walk<WalkOrder::PreOrder>([&](secret::GenericOp genericOp) {
     int maxKeySwitchCount = 0;
     int maxCiphertextCount = 0;
+
+    bool isRingDimensionSet = ringDimension != 0;
 
     genericOp.getBody()->walk<WalkOrder::PreOrder>([&](Operation *op) {
       if (op->getNumResults() == 0) {
@@ -204,45 +235,100 @@ void annotateCountParams(Operation *top, DataFlowSolver *solver,
     auto multiplicativeDepth = maxLevel;
     auto numPrimes = multiplicativeDepth + 1;
 
-    // Compute param sizes for HYBRID Key Switching
-    auto phi = ringDimension;
-    auto beta = pow(2.0, 10); // TODO Replace by value set through developer
-    auto t = plaintextModulus;
-    auto D = 6.0;
+    uint32_t numPartQ = ComputeNumLargeDigits(0, multiplicativeDepth);
+    auto auxBits = MAX_BIT_SIZE;  // Max size of a prime number in OpenFHE
+    
+    auto computeLogPQ = [&](int scalingModSize, int firstModSize, int numPrimes) {
+      auto logQ = firstModSize + (numPrimes + 1) * scalingModSize;
+      auto logP = ceil(ceil(static_cast<double>(logQ)  / numPartQ) / auxBits) * auxBits;
 
-    auto vKey = 2.0 / 3.0; // TODO Make adjustable
-    auto vErr = 3.19 * 3.19; // TODO Make adjustable
+      return logP + logQ;
+    };
 
-    auto boundScale = D * t * sqrt((phi / 12.0) * (1.0 + (phi * vKey)));
-
-    auto boundKeySwitch = D * t * phi * sqrt(vErr / 12.0);
-
-    auto K = 100.0;
-    auto P = K * beta * sqrt(log(numPrimes * pow(2.0, 60)) / log(beta));
-
-    auto f0 = beta * sqrt(numPrimes * log2(t * phi)) / P;
-
-    auto vKS = f0 * boundKeySwitch + boundScale;
-
-    auto bOptimal = boundScale + sqrt(boundScale * boundScale + (maxKeySwitchCount * vKS));
-    auto pOptimal = 2.0 * maxCiphertextCount * bOptimal;
-
-    auto scalingModSize = ceil(log2(pOptimal));
-    if (scalingModSize > 60) {
-      top->emitOpError() << "ScalingModSize too large (> 60 bit).\n";
+    auto upperBoundOnModuli = lbcrypto::GetMSB(plaintextModulus) + 28;
+    auto logQP = computeLogPQ(upperBoundOnModuli, upperBoundOnModuli, numPrimes);
+    
+    if (!isRingDimensionSet){
+      ringDimension = 16384;
     }
 
-    double firstModSize = ceil(1 + log2( bOptimal));
-    firstModSize = findValidFirstModSize(firstModSize, ringDimension, plaintextModulus); 
-    if (!firstModSize) {
-      top->emitOpError() << "Cannot find valid prime for scalingModSize\n";
-    };
+    auto firstModSize = 0;
+    auto scalingModSize = 0;
     
+    while (true) {
+      auto newRingDimension = ringDimension;
+      auto startDimension = ringDimension;
+      std::cerr << "Testing Ring Dimension: " << ringDimension << std::endl;
+      // Compute param sizes for HYBRID Key Switching
+      auto boundOptimal = computeOptimalBoundSize(startDimension, plaintextModulus, maxKeySwitchCount, maxCiphertextCount, numPrimes);
+      auto moduliOptimal = ceil(1 + log2(maxCiphertextCount) + boundOptimal);
 
-    scalingModSize = findValidScalingModSize(scalingModSize,firstModSize , numPrimes, ringDimension, plaintextModulus);
-    if (!scalingModSize) {
-      top->emitOpError() << "Cannot find enough valid primes for scalingModSize\n";
-    };
+      scalingModSize = ceil(log2(moduliOptimal));
+      if (scalingModSize > MAX_BIT_SIZE) {
+        top->emitOpError() << "ScalingModSize too large (> 60 bit).\n";
+      }
+
+      firstModSize = ceil(1 + boundOptimal);
+      firstModSize = findValidFirstModSize(firstModSize, startDimension, plaintextModulus); 
+      if (!firstModSize) {
+        top->emitOpError() << "Cannot find valid prime for scalingModSize\n";
+      };
+
+      scalingModSize = findValidScalingModSize(scalingModSize, firstModSize, numPrimes, startDimension, plaintextModulus);
+      if (!scalingModSize) {
+        top->emitOpError() << "Cannot find enough valid primes for scalingModSize\n";
+      };
+
+      logQP = computeLogPQ(scalingModSize, firstModSize, numPrimes);
+      
+      if (isRingDimensionSet) {
+        break;
+      }
+      
+      newRingDimension = lbcrypto::StdLatticeParm::FindRingDim(lbcrypto::HEStd_ternary, lbcrypto::HEStd_128_classic, logQP);
+
+      if (newRingDimension != startDimension) {
+        std::cerr << "Set new RingDimension: " << newRingDimension << std::endl;
+        startDimension = newRingDimension;
+      } else {
+        startDimension /= 2;
+        std::cerr << "Try smaller RingDimension: " << startDimension << std::endl;
+        // Try smaller ring dimension
+
+
+        // Compute param sizes for HYBRID Key Switching
+        auto boundOptimal = computeOptimalBoundSize(startDimension, plaintextModulus, maxKeySwitchCount, maxCiphertextCount, numPrimes);
+        auto moduliOptimal = ceil(1 + log2(maxCiphertextCount) + boundOptimal);
+
+        auto testScalingModSize = ceil(log2(moduliOptimal));
+        if (scalingModSize > MAX_BIT_SIZE) {
+          top->emitOpError() << "ScalingModSize too large (> 60 bit).\n";
+        }
+
+        auto testFirstModSize = ceil(1 + boundOptimal);
+        testFirstModSize = findValidFirstModSize(testFirstModSize, startDimension, plaintextModulus); 
+        if (!testFirstModSize) {
+          top->emitOpError() << "Cannot find valid prime for scalingModSize\n";
+        };
+
+        testScalingModSize = findValidScalingModSize(testScalingModSize, testFirstModSize, numPrimes, startDimension, plaintextModulus);
+        if (!testScalingModSize) {
+          top->emitOpError() << "Cannot find enough valid primes for scalingModSize\n";
+        };
+
+        logQP = computeLogPQ(scalingModSize, firstModSize, numPrimes);
+        newRingDimension = lbcrypto::StdLatticeParm::FindRingDim(lbcrypto::HEStd_ternary, lbcrypto::HEStd_128_classic, logQP);
+        
+        if (newRingDimension == startDimension) {
+          std::cerr << "Smaller possible. Set new RingDimension: " << newRingDimension << std::endl;
+          startDimension = newRingDimension;
+        } else {
+          std::cerr << "Smaller not possible. Final RingDimension: " << 2 * startDimension << std::endl;
+          ringDimension = 2 * startDimension;
+          break;
+        }
+      }
+    }
 
     // annotate mgmt::OpenfheParamsAttr to func::FuncOp containing the genericOp
     auto *funcOp = genericOp->getParentOp();
@@ -254,3 +340,29 @@ void annotateCountParams(Operation *top, DataFlowSolver *solver,
 
 }  // namespace heir
 }  // namespace mlir
+
+
+
+// int findRingDimension() {
+
+//   while (true) {
+//     int startDimension = 16384;
+
+//     scale, first = computeModuli()
+
+//     logPQ = computeLogPQ 
+//     dimension = computeDimension(logPQ)
+
+//     if (dimension != startDimension) {
+//       startDimension = dimension
+//     }
+
+//     if (dimension == startDimension) {
+//       dimension /= 2
+//       scale, first computeModuli()
+//       logPQ = computeLogPQ
+//       dimension = computeDimension(logPQ)
+//       break;
+//     }
+//   }
+// }
