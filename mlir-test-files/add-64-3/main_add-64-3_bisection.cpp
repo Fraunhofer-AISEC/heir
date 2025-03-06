446911c81efec5b8fa1a30e871885bb555e2aef7
cstarn <36639566+CStarn@users.noreply.github.com>
2025-03-06 18:59:40 +0100

    Add test for high number of different additions in different locations

--- mlir-test-files/add-64-3/main_add-64-3_bisection.cpp
+++ mlir-test-files/add-64-3/main_add-64-3_bisection.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <vector>
 
-#include "dot8_bisection.h"
-#include "main_dot8_common.h"
+#include "add-64-3_bisection.h"
+#include "main_add-64-3_common.h"
 
 int main(int argc, char* argv[]) {
     return run(
