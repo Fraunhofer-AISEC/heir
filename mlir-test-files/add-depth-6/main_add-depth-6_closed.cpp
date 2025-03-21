02fabfff7abcf1352a3e2cdcd3d84fae979a65d0
cstarn <36639566+CStarn@users.noreply.github.com>
2025-03-21 15:05:05 +0100

    Refactor tests

--- mlir-test-files/add-depth-6/main_add-depth-6_closed.cpp
+++ mlir-test-files/add-depth-6/main_add-depth-6_closed.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <vector>
 
-#include "add-depth-1_openfhe.h"
-#include "main_add-depth-1_common.h"
+#include "add-depth-6_closed.h"
+#include "main_add-depth-6_common.h"
 
 int main(int argc, char* argv[]) {
   // Check for ignoreComputation flag
@@ -18,8 +18,7 @@ int main(int argc, char* argv[]) {
     func__encrypt__arg1,
     func,
     func__decrypt__result0,
-    "openfhe",
+    "closed",
     ignoreComputation
 );
 }
-
