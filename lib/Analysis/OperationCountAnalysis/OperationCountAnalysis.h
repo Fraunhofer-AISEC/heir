5b6d56fac204033363f81876d45825e5c04c51eb
Mark Gall <mark.gall@aisec.fraunhofer.de>
2025-05-30 15:46:42 +0200

    manual transfer of changes bc of merges blocking rebase

---
+++ lib/Analysis/OperationCountAnalysis/OperationCountAnalysis.h
@@ -0,0 +1,139 @@
+#ifndef LIB_ANALYSIS_OPERATIONCOUNT_OPERATIONCOUNTANALYSIS_H_
+#define LIB_ANALYSIS_OPERATIONCOUNT_OPERATIONCOUNTANALYSIS_H_
+
+
+#include <cstdint>
+#include "lib/Analysis/SecretnessAnalysis/SecretnessAnalysis.h"
+#include "mlir/include/mlir/Analysis/DataFlow/SparseAnalysis.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/Diagnostics.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/Operation.h"    // from @llvm-project
+#include "mlir/include/mlir/IR/Value.h"        // from @llvm-project
+
+namespace mlir {
+namespace heir {
+
+class OperationCount {
+ public:
+  OperationCount()
+      : initialized(false),
+        ciphertextCount(0),
+        keySwitchCount(0),
+        highestLevel(false) {}
+  explicit OperationCount(int ciphertextCount, int keySwitchCount)
+      : initialized(true),
+        ciphertextCount(ciphertextCount),
+        keySwitchCount(keySwitchCount),
+        highestLevel(false) {}
+  explicit OperationCount(int ciphertextCount, int keySwitchCount,
+                          bool highestLevel)
+      : initialized(true),
+        ciphertextCount(ciphertextCount),
+        keySwitchCount(keySwitchCount),
+        highestLevel(highestLevel) {}
+
+  void countOperation(Operation *op);
+
+  int getCiphertextCount() const {
+    assert(isInitialized() && "OperationCount not initialized");
+    return ciphertextCount;
+  }
+
+  int getKeySwitchCount() const {
+    assert(isInitialized() && "OperationCount not initialized");
+    return keySwitchCount;
+  }
+
+  OperationCount incrementKeySwitch() const {
+    assert(isInitialized() && "OperationCount not initialized");
+    return OperationCount(ciphertextCount, keySwitchCount + 1, highestLevel);
+  }
+
+  bool isInitialized() const { return initialized; }
+
+  bool isHighestLevel() const { return highestLevel; }
+
+  bool operator==(const OperationCount &rhs) const {
+    return initialized == rhs.initialized && ciphertextCount == rhs.ciphertextCount &&
+           keySwitchCount == rhs.keySwitchCount && highestLevel == rhs.highestLevel;
+  }
+
+  OperationCount operator+(const OperationCount &rhs) const {
+    assert(isInitialized() && rhs.isInitialized() &&
+           "OperationCount not initialized");
+    return OperationCount(ciphertextCount + rhs.ciphertextCount,
+                          std::max(keySwitchCount, rhs.keySwitchCount), highestLevel && rhs.highestLevel );
+  }
+
+  static OperationCount max(const OperationCount &lhs, const OperationCount &rhs) {
+    assert(lhs.isInitialized() && rhs.isInitialized() &&
+           "OperationCount not initialized");
+    return OperationCount(std::max(lhs.ciphertextCount, rhs.ciphertextCount),
+                          std::max(lhs.keySwitchCount, rhs.keySwitchCount), 
+                          lhs.highestLevel && rhs.highestLevel);
+  }
+
+  static OperationCount join(const OperationCount &lhs,
+                             const OperationCount &rhs) {
+    if (!lhs.isInitialized()) {
+      return rhs;
+    }
+
+    if (!rhs.isInitialized()) {
+      return lhs;
+    }
+
+    return OperationCount::max(lhs, rhs);
+  }
+
+  void print(llvm::raw_ostream &os) const {
+    if (isInitialized()) {
+      os << "OperationCount(ciphertextCount=" << ciphertextCount
+         << ", keySwitchCount=" << keySwitchCount << ")";
+    } else {
+      os << "OperationCount(uninitialized)";
+    }
+  }
+
+  friend llvm::raw_ostream &operator<<(llvm::raw_ostream &os,
+                                       const OperationCount &count) {
+    count.print(os);
+    return os;
+  }
+
+ private:
+  bool initialized;
+  // "Number of ciphertexts with base noise" that flow into the respective value
+  int ciphertextCount;
+  int keySwitchCount;
+  // Indicates, whether the OperationCount started from a fresh ciphertext
+  bool highestLevel;
+};
+
+class OperationCountLattice : public dataflow::Lattice<OperationCount> {
+ public:
+  using Lattice::Lattice;
+};
+
+class OperationCountAnalysis
+    : public dataflow::SparseForwardDataFlowAnalysis<OperationCountLattice>,
+      public SecretnessAnalysisDependent<OperationCountAnalysis> {
+ public:
+  using SparseForwardDataFlowAnalysis::SparseForwardDataFlowAnalysis;
+  friend class SecretnessAnalysisDependent<OperationCountAnalysis>;
+
+  LogicalResult visitOperation(
+      Operation *op, ArrayRef<const OperationCountLattice *> operands,
+      ArrayRef<OperationCountLattice *> results) override;
+
+  void setToEntryState(OperationCountLattice *lattice) override {
+    propagateIfChanged(lattice, lattice->join(OperationCount()));
+  }
+};
+
+void annotateCountParams(Operation *top, DataFlowSolver *solver,
+                         int ringDimension, int plaintextModulus, std::string algorithm);
+
+}  // namespace heir
+}  // namespace mlir
+
+#endif  // LIB_ANALYSIS_OPERATIONCOUNT_OPERATIONCOUNTANALYSIS_H_
--- tools/heir-opt.cpp
+++ tools/heir-opt.cpp
@@ -57,6 +57,7 @@
 #include "lib/Pipelines/PipelineRegistration.h"
 #include "lib/Transforms/AddClientInterface/AddClientInterface.h"
 #include "lib/Transforms/AnnotateModule/AnnotateModule.h"
+#include "lib/Transforms/AnnotateParameters/AnnotateParameters.h"
 #include "lib/Transforms/AnnotateSecretness/AnnotateSecretness.h"
 #include "lib/Transforms/ApplyFolders/ApplyFolders.h"
 #include "lib/Transforms/CompareToSignRewrite/CompareToSignRewrite.h"
@@ -274,6 +275,7 @@ int main(int argc, char** argv) {
   registerConvertToCiphertextSemanticsPasses();
   registerDropUnitDims();
   registerAnnotateModulePasses();
+  registerAnnotateParametersPasses();
   registerAnnotateSecretnessPasses();
   registerApplyFoldersPasses();
   registerForwardInsertToExtractPasses();
